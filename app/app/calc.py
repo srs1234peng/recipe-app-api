69f944a6c85b54c1e19f5f5db0c57e5fd1bb055d
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-02-20 18:38:32 -0800

    modified:   Dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""""
+Calculation functions
+"""
+
+def add(x, y):
+    """Add two numbers and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y from x and return result."""
+    return y - x
